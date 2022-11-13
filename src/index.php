43cb5f7c7152df16083610473d810c1b00f9899f
devops2200 <zaicev23@gmail.com>
2022-11-13 23:10:24 +0000

    Update index.php file

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@
               <p>
   
                <b>
-                   <font color="red">Application Version 1.1
+                   <font color="red">Application Version 1.2
                  </b>
                    </font>
               </p>
