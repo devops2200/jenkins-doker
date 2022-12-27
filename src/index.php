696d5303a12ee6de9480d96fd337c87f808de64f
devops2200 <118130760+devops2200@users.noreply.github.com>
2022-12-27 16:54:23 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
     <head>
     </head>
       <body>
-        <h1>Jenkins Docker test image</h1>
+        <h1>GCP Cloud Build Prod test image</h1>
  <?php
         $ip_server = $_SERVER['SERVER_ADDR'];
   
